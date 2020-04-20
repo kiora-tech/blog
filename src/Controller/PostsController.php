4e36d0a2327b7d646d16d93a1ca60ad38c5f6738
Rathgeber Stéphane <stephane.kiora@gmail.com>
2020-04-20 23:54:31 +0200

    Add crud

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,16 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/posts",name="posts")
+ */
 class PostsController extends AbstractController
 {
     /**
-     * @Route("/posts/{id}", name="posts")
+     * @Route("/", methods={"GET"})
+     * @param int $id
+     * @return Response
      */
-    public function index($id)
+    public function list(ArticleRepository $repository): Response
     {
-        return $this->render('posts/index.html.twig', compact('id'));
+        return $this->render('posts/index.html.twig', ['articles'=> $repository->findAll()]);
+    }
+
+    /**
+     * @Route("/{id}", name="_show", methods={"GET"}, requirements={"id": "\d+"})
+     * @param Article $article
+     * @return Response
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('posts/show.html.twig', compact('article'));
+    }
+
+    /**
+     * @Route("/test")
+     * @return Response
+     */
+    public function test(): Response
+    {
+        return $this->render('posts/test.html.twig');
     }
 }
